4602f795faedd61f059cfa3e3a4e6d3ba5887e55
Krzysztof Dutka <krzysztofdutka@rocketmail.com>
2017-02-22 16:58:40 +0000

    spirally upwards

--- 3_geometric_pattern/main.js
+++ 3_geometric_pattern/main.js
@@ -1,46 +1,53 @@
 var num_circles = 12;
+
 var circleDiameter;
 var circleRadius;
+
 var rVal;
 var gVal;
 var bVal;
 
 function setup() {
     createCanvas(480, 600);
+    
+    frameRate(15)
+    
     circleDiameter = width/num_circles;
     circleRadius = circleDiameter/2
+    
+    rVal = 255;
+    gVal = 0;
+    bVal = 0;
 }
 
 function draw() {
-    rVal = 255;
-    gVal = 0;
-    bVal = 1;
-    
     var isShifted = false;
     
     var y = height;
     
     while (y >= 0){
-    
+         
+         var x;
+         
         if (isShifted){
-            x = circleRadius
+            x = circleRadius;
         } else {
-            x = 0
+            x = 0;
         }
         
          while (x <= width) {
-             fill(color(rVal, gVal, bVal))
-             stroke(color(rVal, gVal, bVal))
-           ellipse(x, y, circleDiameter, circleDiameter);
+            fill(color(rVal, gVal, bVal));
+            stroke(color(rVal, gVal, bVal));
+            ellipse(x, y, 1.5 * circleDiameter, 0.75 * circleDiameter);
         
-           x = x + circleDiameter
+            x = x + 2 * circleDiameter;
         }
         
-        y = y - circleRadius
+        y = y - 3 / 10 * circleRadius;
         isShifted = !isShifted;
         
-        rVal = rVal - 2;
-        gVal = gVal + 7;
-        bVal = bVal + 3;
+        rVal = (rVal + 5) % 256;
+        gVal = (gVal + 0) % 256;
+        bVal = (bVal + 55) % 256;
     }
 }
