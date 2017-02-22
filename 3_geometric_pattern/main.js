03ac6d4277de8d078e6e5d9eef1c22229f8b1548
Krzysztof Dutka <krzysztofdutka@rocketmail.com>
2017-02-22 16:32:06 +0000

    Add color and scale directioning

--- 3_geometric_pattern/main.js
+++ 3_geometric_pattern/main.js
@@ -1,22 +1,46 @@
 var num_circles = 12;
 var circleDiameter;
+var circleRadius;
+var rVal;
+var gVal;
+var bVal;
 
 function setup() {
     createCanvas(480, 600);
     circleDiameter = width/num_circles;
+    circleRadius = circleDiameter/2
 }
 
 function draw() {
-    var y = 0;
-    while (y <= height){
+    rVal = 255;
+    gVal = 0;
+    bVal = 1;
     
-        var x = 0;
+    var isShifted = false;
+    
+    var y = height;
+    
+    while (y >= 0){
+    
+        if (isShifted){
+            x = circleRadius
+        } else {
+            x = 0
+        }
+        
          while (x <= width) {
+             fill(color(rVal, gVal, bVal))
+             stroke(color(rVal, gVal, bVal))
            ellipse(x, y, circleDiameter, circleDiameter);
         
            x = x + circleDiameter
         }
         
-        y = y + circleDiameter
+        y = y - circleRadius
+        isShifted = !isShifted;
+        
+        rVal = rVal - 2;
+        gVal = gVal + 7;
+        bVal = bVal + 3;
     }
 }
